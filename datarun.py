253d9e035c474e8b37cf09daea2a16399102861f
David Lewis <lewisde@gmail.com>
2016-04-02 13:44:51 -0400

    Added time stamps and command line arguments. Output now named after input parameters for duplication of runs.

--- datarun.py
+++ datarun.py
@@ -8,7 +8,7 @@
 import pyopencl as cl
 import numpy
 import sys
-from time import time
+from time import time, gmtime, strftime
 
 
 def pfunc(r):
@@ -19,7 +19,7 @@ def pfunc(r):
                 c_result[i] *= c_result[i]
                 c_result[i] = c_result[i] * (a[i] / 2.0)
     time2 = time()
-    print(time2 - time1)
+ #   print(time2 - time1)
     return time2 - time1
 
 
@@ -39,31 +39,41 @@ def clfunc(device, kern):
     exec_evt = prg.sum(queue, a.shape, None, a_buf, b_buf, dest_buf)
     exec_evt.wait()
     elapsed = 1e-9 * (exec_evt.profile.end - exec_evt.profile.start)
-    print(elapsed)
+ #   print(elapsed)
     return elapsed
+start_time = time()
+start_str = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+print('Start time: {}, run from {} to {}'.format(start_str, sys.argv[1], sys.argv[2]))
 
-
-for i in range(10, 100):
-    print(i)
+if len(sys.argv) == 4:
+    step = int(sys.argv[3])
+else:
+    step = 1
+    
+for i in range(int(sys.argv[1]), int(sys.argv[2]), step):
+#    print(i)
     run = []
     a = numpy.random.rand(i).astype(numpy.float32)
     b = numpy.random.rand(i).astype(numpy.float32)
     c_result = numpy.empty_like(a)
-    kernel = "__kernel void sum(__global const float *a,__global const float \
-    *b, __global float *c){int loop;int gid = get_global_id(0);for(loop=0; \
-    loop<" + str(i) + ";loop++){for(int loop2=0;loop<" + str(i) + ";loop2++) \
-    {c[gid] = a[gid] + b[gid];c[gid] = c[gid]* (a[gid] + b[gid]);c[gid] = \
-    c[gid] *(a[gid] / 2);}}}"
+    kernel1 = "__kernel void sum(__global const float *a,__global const float *b, __global float *c){int loop;int gid = get_global_id(0);for(loop=0; loop<"
+    kernel2 = ";loop++){for(int i=0;i<"
+    kernel3 = ";i++){c[gid] = a[gid] + b[gid];c[gid] = c[gid]* (a[gid] + b[gid]);c[gid] = c[gid] * a[gid] / 2;}}}"
+    kernel = kernel1 + str(i) + kernel2 + str(i) + kernel3
 
     run.append(pfunc(i))
 
     for platform in cl.get_platforms():
         for device in platform.get_devices():
-            print("Device type:", cl.device_type.to_string(device.type))
+#            print("Device type:", cl.device_type.to_string(device.type))
             run.append(clfunc(device, kernel))
 
-    with open('datarun.out', 'w') as writer:
-        string = ''
-        for point in run:
-            string += point + ' '
+    with open('run-{}-{}-{}.out'.format(sys.argv[1], sys.argv[2], step), 'a+') as writer:
+        string = '{}, {}, {}, {}\n'.format(i, run[0], run[1], run[2])
         writer.write(string)
+        print(string)
+        
+end_time = time()
+end_str = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+print('End time: {}, run from {} to {}'.format(end_str, sys.argv[1], sys.argv[2]))  
+print('Elapsed time in seconds:', end_time - start_time)
